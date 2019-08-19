f59896a6307986eee5edeab3128246023547addf
Summers, Randall J <rsummers@ou.edu>
2019-08-19 15:43:33 -0500

    Rewrite tukey window to make last point 0.

--- src/RFTools.jl
+++ src/RFTools.jl
@@ -2,15 +2,16 @@ module RFTools
 
 function tukey(N::Int64, α::Float64)
     window = zeros(Float64, N)
-    boundary1 = α*N/2
-    boundary2 = N*(1-α/2)
-    for n=0:(N-1)
-        if n < boundary1
-            window[n+1] = 0.5*(1 + cos(π*(2*n/(α*N) - 1)))
-        elseif n <= boundary2
-            window[n+1] = 1
+    boundary1 = α/2
+    boundary2 = 1 - α/2
+    x=range(0, length=N, stop=1)
+    for n=1:N
+        if x[n] < boundary1
+            window[n] = 0.5*(1 + cos(2*π/α*(x[n] - α/2)))
+        elseif x[n] < boundary2
+            window[n] = 1
         else
-            window[n+1] = 0.5*(1 + cos(π*(2*n/(α*N) - 2/α + 1)))
+            window[n] = 0.5*(1 + cos(2*π/α*(x[n] - 1 + α/2)))
         end
     end
     window
