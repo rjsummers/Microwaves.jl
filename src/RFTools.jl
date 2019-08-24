5263ecf708ad281fbaf3aa5c5ccf7451e4f3e421
Summers, Randall J <rsummers@ou.edu>
2019-08-24 00:12:58 -0500

    Write more power conversion functions.

--- src/RFTools.jl
+++ src/RFTools.jl
@@ -5,6 +5,26 @@ module RFTools
 include("Networks.jl")
 include("Windows.jl")
 
+function voltage_gain(x::Number, units::Symbol)
+    if units == :power
+        return sqrt(x)
+    elseif units == :db
+        return 10^(x/20)
+    elseif units == :np
+        return exp(x)
+    end
+end
+
+function power_gain(x::Number, units::Symbol)
+    if units == :voltage
+        return x^2
+    elseif units == :db
+        return 10^(x/10)
+    elseif units == :np
+        return 10^(20 * log10(Base.MathConstants.e) * x / 10)
+    end
+end
+
 function db(x::Number)
     20*log10(abs(x))
 end
@@ -18,13 +38,5 @@ function db(x::Number, units::Symbol)
         return 20 * log10(Base.MathConstants.e) * x
     end
 end 
-
-function voltage_gain(x::Number, units::Symbol)
-    if units == :db
-        return 10^(x/20)
-    elseif units == :np
-        return exp(x)
-    end
-end
 
 end # module
