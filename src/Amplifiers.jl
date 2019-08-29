fa3344f3c4eba2e9a2a092d1395df4da03bc7dba
Summers, Randall J <rsummers@ou.edu>
2019-08-28 22:46:28 -0500

    Fix problem with accessing s-parameters.

--- src/Amplifiers.jl
+++ src/Amplifiers.jl
@@ -4,12 +4,16 @@ using ..RF: Networks
 
 function Γ_in(net::Network, Γ_L::Vector{Complex})
     sparams = SParameters(net)
-    sparams[:,1,1] .+ (sparams[:,1,2] .* sparams[:,2,1] .* Γ_L) ./ (1 .- sparams[:,2,2] .* Γ_L)
+    sparams.s[:,1,1] .+ (sparams.s[:,1,2] .* sparams.s[:,2,1] .* Γ_L) ./ (1 .- sparams.s[:,2,2] .* Γ_L)
 end
 
 function Γ_out(net::Network, Γ_S::Vector{Complex})
     sparams = SParameters(net)
-    sparams[:,2,2] .+ (sparams[:,1,2] .* sparams[:,2,1] .* Γ_S) ./ (1 .- sparams[:,1,1] .* Γ_S)
+    sparams.s[:,2,2] .+ (sparams.s[:,1,2] .* sparams.s[:,2,1] .* Γ_S) ./ (1 .- sparams.s[:,1,1] .* Γ_S)
+end
+
+function power_gain(net::Network, Γ_L::Vector{Complex})
+    s = Sparameters(net)
 end
 
 end # module
