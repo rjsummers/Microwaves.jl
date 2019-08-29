7308146c02d82fc9217e9051dd9eb7f2807363b1
Summers, Randall J <rsummers@ou.edu>
2019-08-28 22:30:21 -0500

    Write a function for amp output reflection coeff

--- src/Amplifiers.jl
+++ src/Amplifiers.jl
@@ -7,4 +7,9 @@ function Γ_in(net::Network, Γ_L::Vector{Complex})
     sparams[:,1,1] .+ (sparams[:,1,2] .* sparams[:,2,1] .* Γ_L) ./ (1 .- sparams[:,2,2] .* Γ_L)
 end
 
+function Γ_out(net::Network, Γ_S::Vector{Complex})
+    sparams = SParameters(net)
+    sparams[:,2,2] .+ (sparams[:,1,2] .* sparams[:,2,1] .* Γ_S) ./ (1 .- sparams[:,1,1] .* Γ_S)
+end
+
 end # module
